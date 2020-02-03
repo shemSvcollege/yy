3c66e8d9c1385ed3ec484024b2dfdd4e62b06bb6
shem bar <shembar100@gmail.com>
2020-02-03 15:50:12 +0200

    Updates

---
+++ precache-manifest.48828b33e3d5751751a691bcb557376f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46580c410ae5cd7b308e6bdb31cbe399",
+    "url": "shemSvcollege.github.io/yy/index.html"
+  },
+  {
+    "revision": "04d7da7d74a8c839c63c",
+    "url": "shemSvcollege.github.io/yy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "00a9729c0c52dcca1f70",
+    "url": "shemSvcollege.github.io/yy/static/js/2.56fed729.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "shemSvcollege.github.io/yy/static/js/2.56fed729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d7da7d74a8c839c63c",
+    "url": "shemSvcollege.github.io/yy/static/js/main.4ae70ce7.chunk.js"
+  },
+  {
+    "revision": "08ec5ea7a7b90707bc4b",
+    "url": "shemSvcollege.github.io/yy/static/js/runtime-main.3ad19a7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "shemSvcollege.github.io/yy/static/media/logo.5d5d9eef.svg"
+  }
+]);
